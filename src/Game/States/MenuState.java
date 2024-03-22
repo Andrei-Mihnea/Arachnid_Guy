51237b730895f62e7fde21dcaed9c9f3e093ff1d
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2024-03-22 16:43:52 +0200

    key&mouse handlers + sprites stuff

---
+++ src/Game/States/MenuState.java
@@ -0,0 +1,27 @@
+package Game.States;
+
+import Game.Util.KeyHandler;
+import Game.Util.MouseHandler;
+
+import java.awt.*;
+
+public class MenuState extends GameState {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void input(MouseHandler mouse, KeyHandler key) {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+}
